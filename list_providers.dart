5e98f52268e2a2a3532b3e2f11209f5e51785eea
Victor Hugo Augusto do Valle <tiic@MacBook-Air-de-Victor.local>
2026-07-21 06:16:23 -0300

    Update Instagram Auth credentials to ID 958443413915684

--- list_providers.dart
+++
@@ -1,7 +0,0 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-void main() {
-  print('--- OAuthProvider Names ---');
-  // Usando reflexão simples/iteração manual para evitar crash de compilação FFI
-  OAuthProvider.values.forEach((v) => print(v.name));
-}
